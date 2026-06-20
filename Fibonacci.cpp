8008b451a7bdd8b37284bbc840470b9f50626850
PorebskiCode18 <damianfc37@gmail.com>
2026-06-19 20:58:58 -0500

    reallydone

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -5,31 +5,45 @@
 
 using namespace std;
 
-class Fibonacci {
 
-public:
-    
-    int fib(int n) {
-        static int numberOfCalls = 0;
-        numberOfCalls++;
-        cout << "Call " << numberOfCalls << endl;
-        if (n < 0) {
-            return -1;
-        }
-        if (n <= 1) {
-            return n;
-        }
-        return fib(n - 1) + fib(n - 2);
+int fibRecurssive(int n) {
+    static int numberOfCalls = 0;
+    numberOfCalls++;
+    cout << "Call " << numberOfCalls << endl;
+    if (n < 0) {
+        return -1;
     }
+    if (n <= 1) {
+        return n;
+    }
+    return fibRecurssive(n - 1) + fibRecurssive(n - 2);
 };
+int fibLoop(int n) {
+    if (n < 0) {
+        return -1;
+    }
+    if (n <= 1) {
+        return n;
+    }
+    int a = 0;
+    int b = 1;
+    int c;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;    
+}
 
 int main() {
     int n;
     cin >> n;
+    int result = fibRecurssive(n);
+    cout << "Recursive: The Fibonacci after " << n << " terms is: " << result << endl;
+    int result2 = fibLoop(n);
+    cout << "Loop: The Fibonacci after " << n << " terms is: " << result2 << endl;
 
-    Fibonacci f;
-    int result = f.fib(n);
-    cout << "The Fibonacci of " << n << " is: " << result << endl;
     return 0;
 }
 
